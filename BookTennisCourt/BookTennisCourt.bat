6ab5c855e39bad61f828971e010e9c800a95eb8d
waynelxb <waynelxb@gmail.com>
2022-02-16 09:10:39 -0600

    Refined script
    Refined script

--- BookTennisCourt/BookTennisCourt.bat
+++ BookTennisCourt/BookTennisCourt.bat
@@ -3,4 +3,4 @@ SET court_number=%1
 SET list_appointment_military_hour=%2
 
 
-python C:\Projects\Python\BookTennisCourt\BookTennisCourt.py %court_number% %list_appointment_military_hour%
+python C:\Projects\GitHub\UtilityRepository\BookTennisCourt\BookTennisCourt.py %court_number% %list_appointment_military_hour%
